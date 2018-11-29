c68e58cf4529d8424c379dd8fa122859459502dd
Teun Wassing <teunwassing@gmail.com>
2018-11-29 23:16:03 +0100

    update

--- clusterlights.py
+++ clusterlights.py
@@ -1,21 +1,49 @@
-import pygatt
+#import pygatt
 import logging
-import time
+import json
+import sys
 
-mac = "24:35:CC:09:88:57"
 
-handle = 37
+configFile = "config.json"
 
 logging.basicConfig()
 logging.getLogger('pygatt').setLevel(logging.DEBUG)
+
 # The BGAPI backend will attemt to auto-discover the serial device name of the
 # attached BGAPI-compatible USB adapter.
-adapter = pygatt.GATTToolBackend()
+#adapter = pygatt.GATTToolBackend()
+
+def load_config(filename):
+    return json.load(open(configFile, "r"))
+
+def conf_to_bytearray(confValue):
+    hex_string = confValue.replace(":", "")
+    byte_array = bytearray.fromhex(hex_string)
+    byte_array
+
+def perc_to_hex(maxHex, minHex, percentage):
+    raise NotImplementedError
+
+def parse_command(command, config):
+    if command in config["command"]:
+        if command == "mode":
+           print "henkie"   
+        else:
+            conf_to_bytearray(config["command"][command])
+
+
+
+command = sys.argv[1]
+config = load_config(configFile)
+#bytesToSend = parse_command(command, config)
+
+print command
 
 try:
     adapter.start()
     device = adapter.connect(mac)
-    device.char_write_handle(handle, bytearray([1, 1, 1, 0]))
+    device.char_write_handle(handle, bytesToSend)
 
 finally:
-    adapter.stop()
+    print "henk"
+    #adapter.stop()
